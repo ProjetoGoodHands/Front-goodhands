31489d83f55a8b8efcf05f14aa18ab74fc1ac68f
Roger de castro <rogerd023@hotmail.com>
2023-06-22 10:18:55 -0300

    implementação do login e cadastro

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,32 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import './Login.css';
-import { Grid,Typography, TextField, Button } from '@material-ui/core';
-import {Box,} from '@mui/material';
-import { Link } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { Grid, Box, Typography, TextField, Button } from "@material-ui/core";
+import { Link, useNavigate } from 'react-router-dom';
+import UserLogin from '../../models/UserLogin';
+import { login } from '../../services/Service';
 
 function Login() {
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+    )
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    useEffect(() => {
+        if (token != '') {
+            navigate('/home')
+        }
+    }, [token])
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+            await login(`/usuarios/logar`, userLogin, setToken)
+
+
+            alert('Usuário logado com sucesso!');
+        } catch (error) {
+            alert('Dados do usuário inconsistentes. Erro ao logar!');
+        }
+    }
 
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
-                        <Typography id='typogr' variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight: 'bold'}}>Entre com seu Usuário e Senha</Typography>
-                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                    <form onSubmit={onSubmit}>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                                <Button type='submit' variant='contained' id='logbotao' >
-                                    Logar
-                                </Button>
-                            </Link>
+                            <Button type='submit' variant='contained' color='primary'>
+                                Logar
+                            </Button>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                        <Link to='/cadastro'>
+                            <Typography variant='subtitle1' gutterBottom align='center' style={{ fontWeight: 'bold' }}>Cadastre-se</Typography>
+                        </Link>
                     </Box>
                 </Box>
             </Grid>
@@ -34,7 +72,7 @@ function Login() {
                 backgroundImage: `url(https://stories.freepiklabs.com/api/vectors/fingerprint/bro/render?color=&background=complete&hide=) `,
                 backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '70vh', backgroundSize: 'cover', backgroundPosition: 'center'
             }}>
-     
+
             </Grid>
         </Grid>
     );
