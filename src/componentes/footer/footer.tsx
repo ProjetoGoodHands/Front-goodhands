0c53e33796eb1e0385ba64d00da13605d0a9326b
Roger de castro <rogerd023@hotmail.com>
2023-06-24 19:33:19 -0300

    pequenas mudanças no footer,navbar, home, e service, criação do listar postagem

--- src/componentes/footer/footer.tsx
+++ src/componentes/footer/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import {Typography, Grid } from '@material-ui/core';
-import {Box} from '@mui/material';
+import { Typography, Grid } from '@material-ui/core';
+import { Box } from '@mui/material';
 
 
 function Footer() {
@@ -9,18 +9,18 @@ function Footer() {
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" >
                 <Grid alignItems="center" item xs={12}>
-                    
-                    <hr />
-                    <Box style={{ backgroundColor: "#59D3A5", height: "80px" }} >
+
+
+                    <Box style={{ backgroundColor: "#762075", height: "90px" }} >
                         <Box paddingTop={1}>
                             <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2023 Copyright:</Typography>
                         </Box>
                         <Box>
-                            
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">goodhandsprojetogeneration@gmail.com</Typography>
-                            
+
+                            <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">goodhandsprojetogeneration@gmail.com</Typography>
+
                         </Box>
-                        
+
                     </Box>
                 </Grid>
             </Grid>
