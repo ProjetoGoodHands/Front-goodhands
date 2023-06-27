c564fdd8dfc4d8d9727bce39a9275ccb7c1022d7
Felipe Sousa <felipe.fps09@hotmail.com>
2023-06-27 18:42:34 -0300

    Resolvendo

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,18 +3,20 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: 'https://goodhands.onrender.com/'
 
-    
+
 })
 
-export const cadastroUsuario = async(url:any, dados:any, setDado:any) => {
+export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data)
 }
-export const login = async(url:any, dados:any, setDado:any) => {
+
+export const login = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
-    setDado(resposta.data)
+    setDado(resposta.data.token)
 }
-export const busca = async(url:any, dados:any, setDado:any) => {
-    const resposta = await api.post(url, dados)
+
+export const busca = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
     setDado(resposta.data)
 }
