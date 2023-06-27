a5adf7d459596d7c3a72d2408a4e1cef50296466
Felipe Sousa <felipe.fps09@hotmail.com>
2023-06-27 20:02:31 -0300

    Resolvendo problema do Gustavo

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ import Postagem from './pages/home/postagem';
 import Home from './pages/inicio/home';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import Contato from './pages/contato/Contato';
+import ListaTema from './componentes/temas/listatema/ListaTema';
+import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
+import CadastroPostagem from './componentes/postagens/cadastrarPostagem/CadastrarPostagem';
+import CadastroTema from './componentes/temas/cadastrarTema/CadastrarTema';
+import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
+import CadastrarTema from './componentes/temas/cadastrarTema/CadastrarTema';
 
 
 function App() {
@@ -18,12 +25,20 @@ function App() {
         <Routes> // Antigo Switch
 
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/postagem" element={<Postagem />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
           <Route path="/sobre" element={<Sobrenos />} />
           <Route path="/cadastro" element={<CadastroUsuario />} />
-          <Route path="/contato" element={<Contato />} />
+          <Route path="/posts" element={<ListaPostagem />} />
+          <Route path="/formularioPostagem" element={<CadastroPostagem />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPostagem />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastrarTema />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+          
         </Routes>
       </div>
       <Footer />
